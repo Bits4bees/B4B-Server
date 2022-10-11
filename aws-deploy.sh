99160bb2b45b40d193fca5dc078a539a6c5a9d5e
jgonzalezPernix <jgonzalez@pernixlabs.com>
2022-10-11 15:48:16 -0600

    Actualizar script

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -4,7 +4,11 @@ sudo apt-get update
 
 wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) 
 sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/local/bin/docker-compose
-sudo chmod -v +x /usr/local/bin/docker-compose 
+sudo chmod -v +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose-Linux-x86_64;
 
 sudo usermod -a -G docker ec2-user
+sudo usermod -aG docker $USER
+
+
 docker-compose -f services/docker_compose_production.yml up -d
