ead0fc18c7e8579e24f5be08debf2aef32d8d8be
jgonzalezPernix <jgonzalez@pernixlabs.com>
2022-10-11 15:16:08 -0600

    Actualizar script

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
+
+echo "------------THIS IS THE USER-------------"
+
+echo "$USER"
+
 docker-compose -f services/docker_compose_production.yml up -d
