677ff9c9237c3b7c0348631155afeccacd064811
esteariaslopez <esteariaslopez@gmail.com>
2022-10-11 21:01:24 -0600

    Update aws-deploy.sh

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -3,8 +3,9 @@ source /home/ec2-user/.bash_profile
 sudo yum update -y
 
 sudo usermod -a -G docker ec2-user
+sudo usermod -aG docker $USER
 
 echo "------------ USUARIO -------------"
 echo "$USER"
 
-docker-compose -f services/docker_compose_production.yml up -d
+docker-compose -f ~/services/docker_compose_production.yml up -d
