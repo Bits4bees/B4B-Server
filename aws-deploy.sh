67e4c6a71f502d8a0b293e3e02244db4c8a777a6
jgonzalezPernix <jgonzalez@pernixlabs.com>
2022-10-17 20:23:10 -0600

    Actualizar .sh

--- aws-deploy.sh
+++ aws-deploy.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 source /home/ec2-user/.bash_profile
-sudo yum update -y
 
-sudo usermod -a -G docker ec2-user
-sudo usermod -aG docker $USER
+#sudo yum update -y
 
-echo "########## Cambiando a carpeta donde se copia el repositorio ##########"
+#sudo usermod -a -G docker ec2-user
+#sudo usermod -aG docker $USER
 
-cp /home/ec2-user/.env /home/ec2-user/bits4bees
-cd /home/ec2-user/bits4bees
+#echo "########## Cambiando a carpeta donde se copia el repositorio ##########"
 
-echo "------------ Carpeta actual -------------"
-pwd
-echo "------------ Carpeta actual -------------"
+echo "########## Copiando .envo a services ##########"
+cp /home/ec2-user/credentials/.env ./services
 
-echo "------------ Contenido -------------"
-ls
-echo "------------ Contenido -------------"
+echo "########## Copiando .envo a /app ##########"
+cp /home/ec2-user/credentials/.env ./app
 
+#cd /home/ec2-user/bits4bees
+
+echo "########## Entrando a /services ##########"
+cd /services
+
+#echo "------------ Carpeta actual -------------"
+#pwd
+#echo "------------ Carpeta actual -------------"
+
+#echo "------------ Contenido -------------"
+#ls
+#echo "------------ Contenido -------------"
+
+echo "########## Ejecutando docker compose ##########"
+
+
+sudo docker build -t nuxt_app app/
 docker-compose -f ./services/docker_compose_production.yml up -d
